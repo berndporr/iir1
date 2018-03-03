7797f08fb794d139005b36acd040e504bf3e3c94
Bernd Porr <mail@berndporr.me.uk>
2018-03-03 23:56:16 +0000

    Added tests for bessel and elliptic

---
+++ test/bessel.cpp
@@ -0,0 +1,42 @@
+#include "Iir.h"
+
+#include <stdio.h>
+
+#include "assert_print.h"
+
+int main (int,char**)
+{
+	const int order = 3;
+	Iir::Bessel::LowPass<order> f;
+	const float samplingrate = 1000; // Hz
+	const float cutoff_frequency = 5; // Hz
+	f.setup (order, samplingrate, cutoff_frequency);
+	f.reset ();
+	double b;
+	for(int i=0;i<10000;i++) 
+	{
+		float a=0;
+		if (i==10) a = 1;
+		b = f.filter(a);
+		assert_print(!isnan(b),"Lowpass output is NAN\n");
+	}
+	//fprintf(stderr,"%e\n",b);
+	assert_print(fabs(b) < 1E-30,"Lowpass value for t->inf to high!");
+
+	Iir::Bessel::BandStop<order,Iir::DirectFormI> bs;
+	const float center_frequency = 50;
+	const float frequency_width = 5;
+	bs.setup (order, samplingrate, center_frequency, frequency_width);
+	bs.reset ();
+	for(int i=0;i<50000;i++) 
+	{
+		float a=0;
+		if (i==10) a = 1;
+		b = bs.filter(a);
+		assert_print(!isnan(b),"Bandstop output is NAN\n");
+		//fprintf(stderr,"%e\n",b);
+	}
+	//fprintf(stderr,"%e\n",b);
+	assert_print(fabs(b) < 1E-30,"Bandstop value for t->inf to high!");
+	return 0;
+}
