92da0b367b7ad7f27397edbe5ccdb57eea6059b9
Bernd Porr <mail@berndporr.me.uk>
2021-12-05 01:20:20 +0000

    new version in cmake

--- test/state.cpp
+++ test/state.cpp
@@ -4,8 +4,8 @@
 
 #include "assert_print.h"
 
-void assert_float(float a, float b) {
-	const float d = fabs(a-b);
+void assert_double(double a, double b) {
+	const double d = fabs(a-b);
 	if (d < 1E-5) {
 		fprintf(stderr,"%e == %e, err = %e\n",a,b,d);		
 		return;
@@ -22,9 +22,9 @@ void test1 () {
 		{{ 0.02008337,  0.04016673,  0.02008337,
 		   1.,         -1.56101808,  0.64135154}};
         Iir::Custom::SOSCascade<nSOS,Iir::DirectFormI> cust(coeff);
-	assert_float(cust.filter(-1.0f),-2.00833656e-02);
-	assert_float(cust.filter(0.5f),-6.14755450e-02);
-	assert_float(cust.filter(1.0f),-6.30005740e-02);
+	assert_double(cust.filter(-1.0f),-2.00833656e-02);
+	assert_double(cust.filter(0.5f),-6.14755450e-02);
+	assert_double(cust.filter(1.0f),-6.30005740e-02);
 }
 
 void test2 () {
@@ -33,9 +33,9 @@ void test2 () {
 		{{ 0.02008337,  0.04016673,  0.02008337,
 		   1.,         -1.56101808,  0.64135154}};
         Iir::Custom::SOSCascade<nSOS,Iir::DirectFormII> cust(coeff);
-	assert_float(cust.filter(-1.0f),-2.00833656e-02);
-	assert_float(cust.filter(0.5f),-6.14755450e-02);
-	assert_float(cust.filter(1.0f),-6.30005740e-02);
+	assert_double(cust.filter(-1.0f),-2.00833656e-02);
+	assert_double(cust.filter(0.5f),-6.14755450e-02);
+	assert_double(cust.filter(1.0f),-6.30005740e-02);
 }
 
 void test3() {
@@ -46,13 +46,13 @@ void test3() {
 		 {1.00000000e+00,  2.00000000e+00,  1.00000000e+00,
 		  1.00000000e+00,  -1.51824184e+00, 7.03962657e-01}};
         Iir::Custom::SOSCascade<nSOS,Iir::DirectFormI> cust(coeff);
-	assert_float(cust.filter(-1.0f),-1.782610e-03);
-	assert_float(cust.filter(0.5f),-1.118353e-02);
-	assert_float(cust.filter(-1.0f),-3.455084e-02);
-	assert_float(cust.filter(0.5f),-7.277369e-02);
-	assert_float(cust.filter(-0.3f),-1.197387e-01);
-	assert_float(cust.filter(3.0f),-1.588640e-01);
-	assert_float(cust.filter(-1E-5f),-1.587363e-01);
+	assert_double(cust.filter(-1.0f),-1.782610e-03);
+	assert_double(cust.filter(0.5f),-1.118353e-02);
+	assert_double(cust.filter(-1.0f),-3.455084e-02);
+	assert_double(cust.filter(0.5f),-7.277369e-02);
+	assert_double(cust.filter(-0.3f),-1.197387e-01);
+	assert_double(cust.filter(3.0f),-1.588640e-01);
+	assert_double(cust.filter(-1E-5f),-1.587363e-01);
 }
 
 void test4() {
@@ -63,13 +63,13 @@ void test4() {
 		 {1.00000000e+00,  2.00000000e+00,  1.00000000e+00,
 		  1.00000000e+00,  -1.51824184e+00, 7.03962657e-01}};
         Iir::Custom::SOSCascade<nSOS,Iir::DirectFormII> cust(coeff);
-	assert_float(cust.filter(-1.0f),-1.782610e-03);
-	assert_float(cust.filter(0.5f),-1.118353e-02);
-	assert_float(cust.filter(-1.0f),-3.455084e-02);
-	assert_float(cust.filter(0.5f),-7.277369e-02);
-	assert_float(cust.filter(-0.3f),-1.197387e-01);
-	assert_float(cust.filter(3.0f),-1.588640e-01);
-	assert_float(cust.filter(-1E-5f),-1.587363e-01);
+	assert_double(cust.filter(-1.0f),-1.782610e-03);
+	assert_double(cust.filter(0.5f),-1.118353e-02);
+	assert_double(cust.filter(-1.0f),-3.455084e-02);
+	assert_double(cust.filter(0.5f),-7.277369e-02);
+	assert_double(cust.filter(-0.3f),-1.197387e-01);
+	assert_double(cust.filter(3.0f),-1.588640e-01);
+	assert_double(cust.filter(-1E-5f),-1.587363e-01);
 }
 
 int main (int,char**)
